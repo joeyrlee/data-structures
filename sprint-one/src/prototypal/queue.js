2e23a2a8a8ccbe34dc579341f0b8dd2abec029f7
Hack Reactor Students <accounts@hackreactor.com>
2016-12-15 11:30:52 -0800

    skeleton set up for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newInstance = Object.create(queueMethods);
+  newInstance.storage = {};
+  newInstance.count = 0;
+  newInstance.deleteCount = 0;
+
+  return newInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
 
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
