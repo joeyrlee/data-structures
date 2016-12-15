d709c44630c8703a94920afcbb038f1da895e08b
Hack Reactor Students <accounts@hackreactor.com>
2016-12-15 15:43:36 -0800

    part one complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}
+  this.count = 0;
+  this.deleteCount = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  var delVal = this.storage[this.deleteCount];
+  delete this.storage[this.deleteCount]
+  this.deleteCount++;
+  return delVal;
+};
+
+Queue.prototype.size = function() {
+  if (this.deleteCount > this.count) {
+    return 0;
+  }
+  return this.count - this.deleteCount;
+};
+
+
 
 
