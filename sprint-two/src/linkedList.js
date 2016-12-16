e97f772aab0da8750bc2cecddddf5e68489facf1
Hack Reactor Students <accounts@hackreactor.com>
2016-12-15 19:53:55 -0800

    link list up to contains working. contains working for head.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,47 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.count = 0;
 
+  //append a new node with the passed-in value to the tail
   list.addToTail = function(value) {
+    //head & tail scenarios:
+    //1. head & tail = null
+    if (list.head === null && list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+    //2. head & tail point to same
+      list.tail = Node(value);
+      //assign next
+      list.head.next = list.tail;
+    }
+
+    list.count++;
   };
 
+  //head points to next node in LL
   list.removeHead = function() {
+    /* Test 4 */
+    // if (list.head.next) {
+    var formerHead = list.head.value;
+    console.log('formerHead: ', formerHead);
+    list.head = list.head.next;
+    list.count--;
+    return formerHead;
   };
 
+  //does stuff
+  //contains works for head only.
   list.contains = function(target) {
+    var currNodeVal = list.head.value;
+    for (var i = 0; i < list.count; i++) {
+      if (list.head.value === target) {
+        return true;
+      }
+      currNodeVal = list.head.next.value;
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +60,8 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//idea:
+//1. on first node instance, head AND tail point to node
+//2. only on second node instance, does head point to node
