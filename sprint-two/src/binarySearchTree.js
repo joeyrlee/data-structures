ac4b35e995ab3cb31b6081d69bdf094e2d8dd7b3
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 11:05:41 -0800

    Analyzed time complexities for binary search tree functions

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -85,3 +85,7 @@ methods.depthFirstLog = function(callBack) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ //insert = O(log(n))
+ //contains = O(log(n))
+ //depthFirstLog = O(n)
