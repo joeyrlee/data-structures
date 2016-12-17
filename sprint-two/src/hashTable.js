51071a9f60d9cc486eb4e10ab1c53b2b7ce0e418
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 15:59:21 -0800

    refactoring and writing hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,13 +9,39 @@ HashTable.prototype.insert = function(k, v) {
   //limitedarray is our storage
   //LA[index] = [key, value]  
   // debugger;
+
+
+  //invoke hash function to find which bucket to drop pair into
   var index = getIndexBelowMaxForKey(k, this._limit);
-  console.log(typeof index)
-  this._storage.set(index, v);
+  //go to bucket or make a new [] bucket at retrieved index
+  var bucket = this._storage[index] || [];
+  //search bucket to see if pair already exists
+    //if key is found in bucket, deposit value (overwrites if already present)
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      var oldValue = bucket[i][1];
+      bucket[i][1] = v;
+      return oldValue;
+    }
+  }
+  //if key isn't present, create a new tuple in bucket
+  bucket.push([k, v]);  
+
+  //return updated bucket into storage
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
+  //invoke hash function to find which bucket to drop pair into
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+
+
+
+
+
+
+  
   return this._storage.get(index)
 };
 
