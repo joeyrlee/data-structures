5bae792ff2d135ee266d0f92c5b9c842889de3e1
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 10:57:27 -0800

    binary tree passing all but last test

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -2,52 +2,64 @@ var BinarySearchTree = function(value) {
   var someInstance = {};
   _.extend(someInstance, methods);
 
-  someInstance.tree = {};
+  // someInstance.tree = {};
 
-  someInstance.tree.value = value;
-  someInstance.tree.left = {};
-  someInstance.tree.right = {};
+  someInstance.value = value;
+  someInstance.left = null;
+  someInstance.right = null;
   return someInstance;
 
 };
 
 var methods = {};
 
-methods.left = function(nodeVal) {
-  return nodeVal.left
-  // body...
-};
-
-methods.right = function(nodeVal) {
-  // body...
-};
-
 methods.insert = function(nodeVal) {
-  var nodeObj = {value: nodeVal, left: {}, right: {}};
   //recursive series of L/R evaluations to determine position
   var searchTree = function(node) {
-    console.log(node)
-    if (node.value === null) {
-      node = nodeObj;
-
-    }
-    if (nodeVal < this.tree.value) {
-      this.tree.left = nodeObj;
-      nodeObj.right = this.tree.left;
-      searchTree(this.tree.left);
-    }
-    if (nodeVal > this.tree.value) {
-      this.tree.right = nodeObj;
-      nodeObj.left = this.tree.right;
-      searchTree(this.tree.right);
+    //make a node variable with properties of a tree
+    var nodeObj = {value: nodeVal, left: null, right: null};
+    //checks left node only when null
+    if (nodeVal < node.value && node.left === null) {
+      node.left = nodeObj;
+    //checks right node only when null
+    } else if (nodeVal > node.value && node.right === null) {
+      node.right = nodeObj;
+    //restart search from left node
+    } else if (nodeVal < node.value && node.left !== null) {
+      searchTree(node.left);
+    //restart search from right node
+    } else if (nodeVal > node.value && node.right !== null) {
+      searchTree(node.right);
     }
   };
-  return searchTree(this.tree);
+  searchTree(this);
 
 };
 
-methods.contains = function(nodeVal) {
-  // body...
+methods.contains = function(target) {
+  //recursive series of L/R evaluations to determine position
+  var result = false;
+  var searchTree = function(node) {
+    //checks left node only when null
+    if (target === node.value) {
+      result = true;
+    } else if (node.right !== null || node.left !== null) {
+      var children = [];
+      if (node.right !== null) {
+        children.push(node.right);
+      }
+      if (node.left !== null) {
+        children.push(node.left);
+      }
+      //for each child node, return the results of searchTree(child)
+      for (var i = 0; i < children.length; i++) {
+        searchTree(children[i]);
+      }
+    }
+    return result;
+  };
+  return searchTree(this);
+  
 };
 
 methods.depthFirstLog = function(callBack) {
