24bac111b9b7918ac680f0367d8c67e1033ff7a0
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 11:40:17 -0800

    deciphering hash table files

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,11 +6,17 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //limitedarray is our storage
+  //LA[index] = [key, value]  
+  // debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  console.log(typeof index)
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(k)
 };
 
 HashTable.prototype.remove = function(k) {
