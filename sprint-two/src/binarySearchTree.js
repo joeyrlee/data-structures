d29b6e65cded65e2269b3fca0f61cfae6e03d7de
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 20:46:31 -0800

    Set passing all tests.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,38 @@
 var BinarySearchTree = function(value) {
+  var someInstance = {};
+  _.extend(someInstance, methods);
+  return someInstance;
+
+};
+
+var methods = {};
+
+methods.left = function(nodeVal) {
+  // body...
+};
+
+methods.right = function(nodeVal) {
+  // body...
+};
+
+methods.insert = function(nodeVal) {
+  //recursive series of L/R evaluations to determine position
+  function searchTree(node) {
+    if (nodeVal < node.value) {
+      // searchTree(node.left);
+    } if (nodeVal > node.value) {
+      // searchTree(node.right);
+    } 
+  }
+  // searchTree(this.storage)
+};
+
+methods.contains = function(nodeVal) {
+  // body...
+};
+
+methods.depthFirstLog = function(callBack) {
+  // body...
 };
 
 
