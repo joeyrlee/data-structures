2852448219fb7e033b66f9549ef7720852b5babb
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 16:28:33 -0800

    Completed Hash Table time complexity analysis and tests all pass.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -14,7 +14,7 @@ HashTable.prototype.insert = function(k, v) {
   //invoke hash function to find which bucket to drop pair into
   var index = getIndexBelowMaxForKey(k, this._limit);
   //go to bucket or make a new [] bucket at retrieved index
-  var bucket = this._storage[index] || [];
+  var bucket = this._storage.get(index) || [];
   //search bucket to see if pair already exists
     //if key is found in bucket, deposit value (overwrites if already present)
   for (var i = 0; i < bucket.length; i++) {
@@ -32,21 +32,32 @@ HashTable.prototype.insert = function(k, v) {
 };
 
 HashTable.prototype.retrieve = function(k) {
-  //invoke hash function to find which bucket to drop pair into
+  //find the index containing the bucket containing k
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-
-
-
-
-
-
-  
-  return this._storage.get(index)
+  //search the bucket for k
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
+  //find the index containing the bucket containing k
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //search bucket for k
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      //remove tuple containing k at tuple[0]
+      bucket.splice(i, 1);
+    }
+  }
+  //once removed from bucket, put bucket back in storage
+  this._storage.set(index, bucket);
 };
 
 
@@ -55,4 +66,7 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+//The time complexity of insert is O(1) on average and then 0(n) as worst case scenarios
+//The time complexity of retrieve is O(1) on average and then O(n) as worst case scenarios
+//The time complexity of remove is O(1) on average and then O(n) as worst case scenarios
 
