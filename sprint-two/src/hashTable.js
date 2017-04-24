f072d9510b5761ec1484614fd242c862dedb1661
Joey Lee <jleepianist@gmail.com>
2017-04-23 21:44:21 -0700

    (refactor) Code cleanup

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,72 +1,208 @@
+// This file contains two distinct solutions:
+//   - The solution presented in lecture
+//   - Refactored version that makes use of higher-order functions
+//
+// The two solutions are delineated by comment blocks.
+// To use the Higher-Order Function (HOF) version, uncomment the
+// line of code near the end of this file.
 
+//////////////////////////////////////////////////////////////////
+// Implementation #1:
+// Solution-Lecture implementation
+//
+// Notice how each function has a similar structure:
+//   - calculate an index
+//   - retreive an bucket at that location
+//   - iterate over the bucket, and
+//     - perform an action if the key is found
+//   - otherwise perform a not-found action
+//////////////////////////////////////////////////////////////////
 
 var HashTable = function() {
-  this._limit = 8;
+
+  this._size = 0;
+    this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
-  //limitedarray is our storage
-  //LA[index] = [key, value]  
-  // debugger;
-
-
-  //invoke hash function to find which bucket to drop pair into
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //go to bucket or make a new [] bucket at retrieved index
+
   var bucket = this._storage.get(index) || [];
-  //search bucket to see if pair already exists
-    //if key is found in bucket, deposit value (overwrites if already present)
+
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      var oldValue = bucket[i][1];
-      bucket[i][1] = v;
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      var oldValue = tuple[1];
+      tuple[1] = v;
       return oldValue;
     }
   }
-  //if key isn't present, create a new tuple in bucket
-  bucket.push([k, v]);  
 
-  //return updated bucket into storage
+  bucket.push([k, v]);
   this._storage.set(index, bucket);
-};
+  this._size++;
+
+  if (this._size > this._limit * 0.75) {
+    this._resize(this._limit * 2);
+  }
+
+  return undefined;
+  };
 
 HashTable.prototype.retrieve = function(k) {
-  //find the index containing the bucket containing k
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  //search the bucket for k
-  var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
+
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      return bucket[i][1];
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      return tuple[1];
     }
   }
+
   return undefined;
-};
+  };
 
 HashTable.prototype.remove = function(k) {
-  //find the index containing the bucket containing k
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //search bucket for k
-  var bucket = this._storage.get(index);
+
+  var bucket = this._storage.get(index) || [];
+
   for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i][0] === k) {
-      //remove tuple containing k at tuple[0]
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
       bucket.splice(i, 1);
+      this._size--;
+      if (this._size < this._limit * 0.25) {
+        this._resize(Math.floor(this._limit / 2));
+      }
+      return tuple[1];
     }
   }
-  //once removed from bucket, put bucket back in storage
-  this._storage.set(index, bucket);
+
+  return undefined;
+  };
+
+
+HashTable.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+
+  // min size of 8, return if nothing to do!
+  newLimit = Math.max(newLimit, 8);
+  if (newLimit === this._limit) { return; }
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(this._limit);
+  this._size = 0;
+
+  oldStorage.each(function(bucket) {
+    if (!bucket) { return; }
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      this.insert(tuple[0], tuple[1]);
+    }
+  }.bind(this));
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-//The time complexity of insert is O(1) on average and then 0(n) as worst case scenarios
-//The time complexity of retrieve is O(1) on average and then O(n) as worst case scenarios
-//The time complexity of remove is O(1) on average and then O(n) as worst case scenarios
 
+//////////////////////////////////////////////////////////////////
+// Implementation #2:
+// Higher-Order Function implementation
+//
+// This version factors out the common code of searching for a
+// tuple in a bucket -- see _tupleSearch. That function performs
+// the specialized work of insert/retreive/remove by delegating
+// to the found and not-found callbacks.
+//////////////////////////////////////////////////////////////////
+
+var HashTableHOF = function() {
+  this._size = 0;
+  this._limit = 8;
+  this._storage = LimitedArray(this._limit);
+};
+
+HashTableHOF.prototype.insert = function(k, v) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      var oldValue = tuple[1];
+      tuple[1] = v;
+      return oldValue;
+    },
+    function(bucket) {
+      bucket.push([k, v]);
+      this._size++;
+      if (this._size > 0.75 * this._limit) {
+        this._resize( this._limit * 2 );
+      }
+    }
+  );
+};
+
+HashTableHOF.prototype.retrieve = function(k) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      return tuple[1];
+    }
+  );
+};
+
+HashTableHOF.prototype.remove = function(k) {
+  return this._tupleSearch(k,
+    function(bucket, tuple, i) {
+      bucket.splice(i, 1);
+      this._size--;
+      if (this._size < 0.25 * this._limit) {
+        this._resize(Math.floor(this._limit / 2));
+      }
+      return tuple[1];
+    }
+  );
+};
+
+HashTableHOF.prototype._tupleSearch = function(key, foundCB, notFoundCB) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  this._storage.set(index, bucket);
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      return foundCB.call(this, bucket, tuple, i);
+    }
+  }
+
+  return notFoundCB ? notFoundCB.call(this, bucket) : undefined;
+};
+
+HashTableHOF.prototype._resize = function(newLimit) {
+  var oldStorage = this._storage;
+
+  // min size of 8, return if nothing to do!
+  newLimit = Math.max(newLimit, 8);
+  if (newLimit === this._limit) { return; }
+
+  this._limit = newLimit;
+  this._storage = LimitedArray(this._limit);
+  this._size = 0;
+
+  oldStorage.each(this._redistribute.bind(this));
+};
+
+HashTableHOF.prototype._redistribute = function(bucket) {
+  if (!bucket) { return; }
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    this.insert(tuple[0], tuple[1]);
+  }
+};
+
+//////////////////////////////////////////////////////////////////
+// uncomment this line to use the HOF version of HashTable
+// HashTable = HashTableHOF;
