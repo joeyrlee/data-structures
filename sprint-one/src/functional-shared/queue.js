84680fdccbb3acbacb4186ad7452cb2ad201a532
Joey Lee <jleepianist@gmail.com>
2017-04-23 21:28:38 -0700

    (refactor) Code Cleanup

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,37 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
   var someInstance = {};
-  someInstance.storage = {};
-  someInstance.count = 0;
-  someInstance.deleteCount = 0;
-  _.extend(someInstance, queueMethods);
+  _(someInstance).extend(queueMethods);
+
+  someInstance._storage = {};
+  someInstance._start = 0;
+  someInstance._end = 0;
 
   return someInstance;
-};
+  };
 
 var queueMethods = {};
 
-//push
+
 queueMethods.enqueue = function(value) {
-  this.storage[this.count] = value;
-  this.count++;
+  this._storage[this._end++] = value;
 };
 
-//shift
 queueMethods.dequeue = function() {
-  var deqVal = this.storage[this.deleteCount];
-  delete this.storage[this.deleteCount];
-  this.deleteCount++;
-  return deqVal;
+  // This does some unnecessary work sometimes. Can you spot why?
+  var result = this._storage[this._start];
+  delete this._storage[this._start];
+
+  this.size() && this._start++;
+
+  return result;
 };
 
-//length
 queueMethods.size = function() {
-  if (this.deleteCount > this.count) {
-    return 0;
-  }
-  return this.count - this.deleteCount;
+  return this._end - this._start;
 };
-
-
